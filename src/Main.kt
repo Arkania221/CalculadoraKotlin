892233938223eb4da49df3e879dbb9a493c60af6
Ángel Borrás <angeloki1963@gmail.com>
2024-02-26 14:02:22 +0100

    creamos las funciones (vacias)

--- src/Main.kt
+++ src/Main.kt
@@ -2,4 +2,54 @@
 ¿Que debemos realizar?: Hay que hacer una calculadora
 con cinco funciones: suma, resta, multiplicacion y division
 ademas de la funcion principal
+
+Realizacion del ejercicio:
+
+    -Realizamos la creacion de las funciones, asi podemos
+    visualizar el trabajo a realizar
+
+    -Como la funcion principal llamara al resto, las demas
+    funciones deben devolver un valor, el cual sera el resultado
+
+    -Para poder devolder un resultado deberan primero operar, para
+    optimizar el codigo, vamos a realizar el input dentro de la
+    funcion principal y se pasaran como parametros a las demas funciones
+
+    -De ultimo se imprimira en pantalla el resultado
+
+    -Para poder realizar varias operaciones dentro del programa
+    sin tener que iniciarlo cada vez, se insertara el main en un
+    bucle
 */
+
+fun Suma() {
+
+
+
+}
+
+fun Resta() {
+
+
+
+}
+
+fun Multiplicacion() {
+
+
+
+}
+
+fun Division() {
+
+
+
+}
+
+
+
+fun Main() {
+
+
+
+}
